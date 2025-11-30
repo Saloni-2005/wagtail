c987974a5a703150ace18a878cfed0238e328044
Saloni-2005 <chaudharysaloni2510@gmail.com>
2025-11-30 23:56:38 +0530

    Created a demo folder

---
+++ demo/demo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7e92(h(&ztbq1_l37o00@)s%0xm#0ubg^bfd$1tgj$-(v+%xp5"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
